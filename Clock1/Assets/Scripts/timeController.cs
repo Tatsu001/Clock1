d403a0ead76608696e11b9ce5e0138cacc1fcde1
Tatsu001 <ytatsu.sub@gmail.com>
2020-04-27 07:03:11 +0900

    スクリプトを編集しました

---
+++ Clock1/Assets/Scripts/timeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;   // DateTimeに必要
+using System.Collections;
+using UnityEngine;
+
+public class timeController : MonoBehaviour {
+
+    public GameObject hour;
+    public GameObject minute;
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+        DateTime dt = DateTime.Now;
+
+        hour.transform.eulerAngles = new Vector3(0,0,-180 + (float)dt.Hour/24*-360 + (float)dt.Minute/60*-15);
+        minute.transform.eulerAngles = new Vector3(0,0,((float)dt.Hour%6)*-60 + -(float)dt.Minute);
+    }
+}
+
